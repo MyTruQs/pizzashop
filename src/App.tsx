8cfc0fffd9deec5dc1ef525e993168bf142bc141
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-09 20:34:38 -0300

    React Helmet (SEO)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { router } from './routes'
 
 export function App() {
-	return <RouterProvider router={router} />
+	return (
+		<HelmetProvider>
+			<Helmet titleTemplate='%s | pizza.shop' />
+			<RouterProvider router={router} />
+		</HelmetProvider>
+	)
 }
