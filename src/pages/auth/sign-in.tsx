4d958909ff94f1b6ff2010be2ef815f00b3d3353
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-14 12:17:45 -0300

    Página: Cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,8 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Ghost } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
 	email: z.string().email(),
@@ -39,6 +41,10 @@ export function SignIn() {
 		<>
 			<Helmet title='Login' />
 			<div className='p-8'>
+				<Button variant='secondary' asChild className='absolute top-8 right-8'>
+					<Link to='/sign-up'>Novo estabelecimento?</Link>
+				</Button>
+
 				<div className='flex w-[350px] flex-col justify-center gap-6'>
 					<div className='flex flex-col gap-2 text-center'>
 						<h1 className='font-semibold text-2xl tracking-tight'>
