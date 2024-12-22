1c356d4438a9f90ea7f9db6d7360eced74ff9b7c
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-21 22:28:13 -0300

    Componente: Detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { TableRow, TableCell } from "@/components/ui/table";
-import { Search, ArrowRight, X } from "lucide-react";
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+import { ArrowRight, Search, X } from 'lucide-react'
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
 	return (
 		<TableRow>
 			<TableCell>
-				<Button variant='outline' size='xs'>
-					<Search className='h-3 w-3' />
-					<span className='sr-only'>Detalhes do pedido</span>
-				</Button>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant='outline' size='xs'>
+							<Search className='h-3 w-3' />
+							<span className='sr-only'>Detalhes do pedido</span>
+						</Button>
+					</DialogTrigger>
+					<OrderDetails />
+				</Dialog>
 			</TableCell>
 			<TableCell className='font-medium font-mono text-xs'>s2d3dso</TableCell>
 			<TableCell className='text-muted-foreground'>há 15 minutos</TableCell>
