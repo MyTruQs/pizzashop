323c4f1aa23071d8ea1f668cd42d2545fdc5bfba
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-21 22:10:20 -0300

    Componente: Paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from '@/components/pagination'
 import {
 	Table,
 	TableBody,
@@ -15,30 +16,33 @@ export function Orders() {
 			<Helmet title='Pedidos' />
 			<div className='flex flex-col gap-4'>
 				<h1 className='font-bold text-3xl tracking-tight'>Pedidos</h1>
-			</div>
-			<div className='space-y-2.5'>
-				<OrderTableFilter />
 
-				<div className='rounded-md border'>
-					<Table>
-						<TableHeader>
-							<TableRow>
-								<TableHead className='w-[64px]' />
-								<TableHead className='w-[140px]'>Identificador</TableHead>
-								<TableHead className='w-[180px]'>Realizado há</TableHead>
-								<TableHead className='w-[140px]'>Status</TableHead>
-								<TableHead>Cliente</TableHead>
-								<TableHead className='w-[140px]'>Total do pedido</TableHead>
-								<TableHead className='w-[164px]' />
-								<TableHead className='w-[132px]' />
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{Array.from({ length: 10 }).map((_, i) => {
-								return <OrderTableRow key={i} />
-							})}
-						</TableBody>
-					</Table>
+				<div className='space-y-2.5'>
+					<OrderTableFilter />
+
+					<div className='rounded-md border'>
+						<Table>
+							<TableHeader>
+								<TableRow>
+									<TableHead className='w-[64px]' />
+									<TableHead className='w-[140px]'>Identificador</TableHead>
+									<TableHead className='w-[180px]'>Realizado há</TableHead>
+									<TableHead className='w-[140px]'>Status</TableHead>
+									<TableHead>Cliente</TableHead>
+									<TableHead className='w-[140px]'>Total do pedido</TableHead>
+									<TableHead className='w-[164px]' />
+									<TableHead className='w-[132px]' />
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								{Array.from({ length: 10 }).map((_, i) => {
+									return <OrderTableRow key={i} />
+								})}
+							</TableBody>
+						</Table>
+					</div>
+
+					<Pagination pageIndex={0} totalCount={105} perPage={10} />
 				</div>
 			</div>
 		</>
