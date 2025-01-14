b8120b37100ec8760d40c1aeb786176b3601bb16
MyTruQs <raulfernandes.rm@gmail.com>
2025-01-14 09:06:42 -0300

    Cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useMutation } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 const signInForm = z.object({
 	email: z.string().email(),
@@ -17,11 +17,15 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+	const [searchParams] = useSearchParams()
+
 	const {
 		register,
 		handleSubmit,
 		formState: { isSubmitting },
-	} = useForm<SignInForm>()
+	} = useForm<SignInForm>({
+		defaultValues: { email: searchParams.get('email') ?? '' },
+	})
 
 	const { mutateAsync: authenticate } = useMutation({
 		mutationFn: signIn,
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -3,9 +3,11 @@ import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
 import { Link, useNavigate } from 'react-router-dom'
 
 const signUpForm = z.object({
@@ -26,14 +28,23 @@ export function SignUp() {
 		formState: { isSubmitting },
 	} = useForm<SignUpForm>()
 
+	const { mutateAsync: registerRestaurantFn } = useMutation({
+		mutationFn: registerRestaurant,
+	})
+
 	async function handleSignUp(data: SignUpForm) {
 		try {
-			await new Promise((resolve) => setTimeout(resolve, 2000))
+			await registerRestaurantFn({
+				restaurantName: data.restaurantName,
+				managerName: data.managerName,
+				email: data.email,
+				phone: data.phone,
+			})
 
 			toast.success('Restaurante cadastrado com sucesso!', {
 				action: {
 					label: 'Login',
-					onClick: () => navigate('/sign-in'),
+					onClick: () => navigate(`/sign-in?email=${data.email}`),
 				},
 			})
 		} catch {
